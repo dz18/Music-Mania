bbee78b8772c29d5322f8a4481a1cce22e036b19
Dylan Zuniga <dylan18zuniga@gmail.com>
2026-05-08 22:42:32 -0700

    updated healthcheck

--- server/controllers/healthcheck.js
+++ server/controllers/healthcheck.js
@@ -1,12 +1,7 @@
 const prisma = require('../prisma/client')
 
 const healthCheck = async(req, res) => {
-  try {
-    await prisma.$queryRaw`SELECT 1`
-    res.status(200).json({ status: 'ok', db: 'connected' })
-  } catch (error) {
-    res.status(500).json({ status: 'error', db: 'disconnected' })
-  }
+  res.status(200).send('ok')
 }
 
 module.exports = { healthCheck }
